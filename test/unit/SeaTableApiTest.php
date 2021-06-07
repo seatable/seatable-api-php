a24d31928bac72218b5f7728a1263ab854ffd026
Tom Klingenberg <tklingenberg@lastflood.net>
2021-06-07 15:01:51 +0200

    test: init unit test
    just a very basic test with its surrounding configuration.
    
    the test shows that the class does not instantiate with empty defaults
    warning-free.
    
    normally it should fail, here the more important part is the creation
    test - to be changed later.

---
+++ test/unit/SeaTableApiTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SeaTable\SeaTableApi;
+
+use PHPUnit\Framework\TestCase;
+use SeaTableAPI;
+
+/**
+ * Base SeaTableApiTest
+ *
+ * @covers SeaTableAPI
+ */
+class SeaTableApiTest extends TestCase
+{
+    public function testCreation()
+    {
+        $this->expectErrorUndefinedArrayKey('url');
+        new SeaTableAPI();
+    }
+
+    private function expectErrorUndefinedArrayKey(string $actual)
+    {
+        $this->expectError();
+        $this->expectErrorMessageMatches(
+            sprintf('~^\QUndefined index: %s\E|\QUndefined array key "%s"\E$~', $actual, $actual)
+        );
+    }
+}
